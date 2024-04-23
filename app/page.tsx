986458827f8edf9683d9c96bf2e1504a022385f8
TommyMac15 <Tmcmullen1@ufl.edu>
2024-04-23 12:37:48 -0400

    ethical considerations

--- app/page.tsx
+++ app/page.tsx
@@ -35,25 +35,47 @@ const Article: React.FC = () => {
         </section>
 
         <section className="mb-4">
-        <h2 className="text-white-600 font-bold text-lg mb-2">Data Privacy</h2>
-        <p>Who is the most at risk? </p>
-        <p>People who need the Neuralink for medical reasons</p>
-        <p>People with spinal or other brain injuries</p>
-        <p>People with neurological disorders</p>
-        <p>Human Testers</p>
+        <h2 className="text-white-600 font-bold text-lg mb-2">Ethical Considerations</h2>
+        <p>Our team realized the pace of neural implant development is rapidly increasing, which led us to consider who is most at risk due to this technology. 
+          This technology has many potential applications but, at the current state, it is mostly aimed to help those with spinal cord injuries. </p>
+          <br></br>
+          <img src="tiny-neural-implant-spinal-injury-control-limbs.webp" alt="Brain Human Interface" className="mx-auto mb-8 rounded-lg" style={{ width: '350px', height: 'auto' }}/>
+          <p>These early adapters are most at risk due to the developing nature of the technology and potential data breaches. </p>
+          <p>
+          Aside from unintentional data breaches, there is also the fact that as technology rapidly advances, oftentimes regulations are slow to keep up. 
+          Regulations regarding the protection of this data is paramount. 
+          There is concern about how HIPAA will apply to this technology and it is likely that laws and regulations will need to adapt to the protection of the vast amount and private nature of this data. 
+            The main issue is that direct-to-consumer products do not need to abide by HIPAA which means that as this becomes available to the general public, the limited protection that HIPAA provides will no longer protect consumer data <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6814252/" style={{ color: '#87CEEB' }}target="_blank">[1]</a>. 
+            There is no current national standard for this neural data protection, leaving individual states to write their own regulations with varying degrees of protection <a href="https://www.vox.com/future-perfect/24078512/brain-tech-privacy-rights-neurorights-colorado-yuste" style={{ color: '#87CEEB' }}target="_blank">[2]</a>.</p>
+          <br></br>
+          <p>Our group believes that there must be a robust regulatory framework established to this emerging issue. 
+            We also believe that this data is too valuable to play catch up with regulations as breaches inevitably occur. </p>
+            <p>
+            For this reason, our group believes that awareness needs to be raised in order to protect the most vulnerable. 
+            We need to ensure that those receiving this implant are aware of the data privacy risks that this imposes. 
+            These early adopters are most at risk and should have the knowledge and agency to make informed decisions about the use of this neural interface technology<a href="https://blogs.ischool.berkeley.edu/w231/2021/07/09/brain-machine-interfaces-and-neuralink-privacy-and-ethical-concerns/" style={{ color: '#87CEEB' }}target="_blank">[3]</a>.
+            They should be aware that letting a device access their brain waves could mean they are risking “identity theft, password hacking, and blackmail” <a href="https://www.fastcompany.com/91029453/unpacking-the-ethical-issues-swirling-around-neuralink" style={{ color: '#87CEEB' }}target="_blank">[4]</a>.</p>
+            <p>It is necessary that Neuralink and other companies in this field are transparent and accountable with the handling, storage, and protection of user data.
+</p>
       </section>
 
       <section className="mb-4">
       
         <h2 className="text-white-600 font-bold text-lg mb-2">Solutions</h2>
       
-        <p>     In order to protect people’s private data one potential solution is to make sure that companies implement strong protocols that in the case the data gets hacked the hacker will not be able to see the data. Luckily we talked with Kostas Hatzianestis, Head of Wireless and System Integration at Precision Neuroscience, and he assured us that “digital data encryption is common in protecting private data.” </p>
+        <p>     In order to protect people’s private data one potential solution is to make sure that companies implement strong protocols that in the case the data gets hacked the hacker will not be able to see the data. 
+          Luckily we talked with Kostas Hatzianestis, Head of Wireless and System Integration at Precision Neuroscience, and he assured us that “digital data encryption is common in protecting private data.” </p>
          <br></br></section>
       <img src="project.png" alt="Brain Human Interface" className="mx-auto mb-8 rounded-lg" style={{ width: '150px', height: 'auto' }}/>     <p>Kostas Hatzianestis - Head of Wireless and System Integration at Precision Neuroscience
 </p>
       <section className="mb-4">
-   <br></br> <p>   However when doing research on he subject we looked at other medical devices that also store data like Pacemakers. Pacemakers are subject to strict regulations to ensure confidentiality and prevent unauthorized access. They must adhere to laws such as HIPAA (Health Insurance Portability and Accountability Act). The Neuralink however, since it is so new, has not had any sort of regulations placed on it. Due to the fact that the implant will be on the brian it will have access to very sensitive information so it is important that regulations are put in place to protect the users. </p>
-          <br></br><p>   Another thing that can help solve this ethical dilemma is for Neurolink to be transparent with their users and inform them of everything that they plan to collect, and do with the data. The customers should be fully informed and given the choice to not have their data collected or be able to opt out at any time.
+   <br></br> <p>   However when doing research on he subject we looked at other medical devices that also store data like Pacemakers. 
+    Pacemakers are subject to strict regulations to ensure confidentiality and prevent unauthorized access. 
+    They must adhere to laws such as HIPAA (Health Insurance Portability and Accountability Act). 
+    The Neuralink however, since it is so new, has not had any sort of regulations placed on it. 
+    Due to the fact that the implant will be on the brian it will have access to very sensitive information so it is important that regulations are put in place to protect the users. </p>
+          <br></br><p>   Another thing that can help solve this ethical dilemma is for Neuralink to be transparent with their users and inform them of everything that they plan to collect, and do with the data. 
+            The customers should be fully informed and given the choice to not have their data collected or be able to opt out at any time.
 </p>
         
 <br></br>
@@ -76,7 +98,11 @@ const Article: React.FC = () => {
 
       <section className="mb-4">
         <h2 className="text-white-600 font-bold text-lg mb-2">References</h2>
-        <p>Links</p>
+        <p>{"[1] https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6814252/"}</p>
+        <p>{"[2] https://www.vox.com/future-perfect/24078512/brain-tech-privacy-rights-neurorights-colorado-yuste"}</p>
+        <p>{"[3] https://blogs.ischool.berkeley.edu/w231/2021/07/09/brain-machine-interfaces-and-neuralink-privacy-and-ethical-concerns/"}</p>
+        <p>{"[4] https://www.fastcompany.com/91029453/unpacking-the-ethical-issues-swirling-around-neuralink"}</p>
+
         <p>People contacted</p>
         
       </section>
